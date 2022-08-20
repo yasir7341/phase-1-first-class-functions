377d30387334780f4d06f8c59352e92952f0a3e2
yasir7341 <yasirabdulrahman75@gmail.com>
2022-08-20 16:40:13 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function eveningRoutine(exercise) {
+    let supper;
+  
+    if (exercise === stretchThebody) {
+      supper = "squashed potatoes";
+    } else if (exercise === runFortyLaps) {
+      supper = "stewed rice";
+    } else {
+      supper = "guava juice";
+    }
+    
